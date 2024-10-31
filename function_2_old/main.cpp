d2df8ed509e249e071b794c5d11270be80e2f4ca
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-31 21:41:50 +0200

    completed

--- function_2_old/main.cpp
+++ function_2_old/main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 using namespace std;
 
+void countElements(int arr[], int size, int& positive, int& negative, int& zero) {
+    positive = 0;
+    negative = 0;
+    zero = 0;
+
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] > 0) {
+            ++positive;
+        }
+        else if (arr[i] < 0) {
+            ++negative;
+        }
+        else {
+            ++zero;
+        }
+    }
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+   
+    int* arr = new int[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    int positive, negative, zero;
+    countElements(arr, n, positive, negative, zero);
+
+    cout << "Positive: " << positive << ", Negative: " << negative << ", Zero: " << zero << endl;
+
+    
+    delete[] arr;
+
+    return 0;
 }

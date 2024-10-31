4a05992dd0ef63f8278fb0d77516989e0714af07
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-31 21:40:18 +0200

    completed

--- function_2_old/main.cpp
+++ function_2_old/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 
+double average(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += arr[i];
+    }
+    return static_cast<double>(sum) / size; 
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+   
+    int* arr = new int[n]; 
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    cout << "Average: " << average(arr, n) << endl;
+
+    delete[] arr;
+    return 0;
 }
